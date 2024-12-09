277940fad58a674ef578878cc51907d5f80ef5bb
HongChan Yoon <hongchani0324@gmail.com>
2024-12-09 20:34:49 +0900

    [feat]: docker postgresql 정상 연결 확인. main class에 추가 애노테이션 설정. - application-secret.yml을 통해 중요 키 따로 관리

--- com.msa_delivery.auth/src/main/java/com/msa_delivery/auth/AuthApplication.java
+++ com.msa_delivery.auth/src/main/java/com/msa_delivery/auth/AuthApplication.java
@@ -2,8 +2,12 @@ package com.msa_delivery.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AuthApplication {
 
 	public static void main(String[] args) {
