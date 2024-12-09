7aabaa14f7f9874855b31c3a03f7ca9271ccba4a
yewonkim301 <yeowne301@gmail.com>
2024-12-09 17:12:01 +0900

    chore: 슬랙메시지 서비스 프로젝트 생성

---
+++ com.msa_delivery.slack_msg/src/main/java/com/msa_delivery/slack_msg/SlackMsgApplication.java
@@ -0,0 +1,13 @@
+package com.msa_delivery.slack_msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackMsgApplication.class, args);
+	}
+
+}
