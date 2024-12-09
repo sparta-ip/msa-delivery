0f3d0f903cf7629c95ac9b1fba9ba7dc572086db
HongChan Yoon <hongchani0324@gmail.com>
2024-12-10 00:23:30 +0900

    [feature] #7: gateway 서버 filter 구현 - Feignclient를 이용해 동기형식으로 auth 서버에 validate 검증 예정 - pre, post 필터 적용.

---
+++ com.msa_delivery.gateway/src/main/java/com/msa_delivery/gateway/config/FeignConfig.java
@@ -0,0 +1,20 @@
+package com.msa_delivery.gateway.config;
+
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// FeignClient 를 Lazy Load 할 경우 HttpMessageConverters 문제로 아래 config 설정이 필요
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Decoder feignDecoder() {
+
+        ObjectFactory<HttpMessageConverters> messageConverters = HttpMessageConverters::new;
+        return new SpringDecoder(messageConverters);
+    }
+}
