498e1764560c9aa018b3c24f65b7cf2bd4b76c6b
HongChan Yoon <hongchani0324@gmail.com>
2024-12-10 14:25:55 +0900

    [feature] #8: AuthController.java에 회원가입, 로그인 api 구현 - #8 - AuthController.java에 회원가입, 로그인 api 구현

---
+++ com.msa_delivery.auth/src/main/java/com/msa_delivery/auth/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.msa_delivery.auth.controller;
+
+import com.msa_delivery.auth.application.dtos.ApiResponseDto;
+import com.msa_delivery.auth.application.dtos.AuthRequestDto;
+import com.msa_delivery.auth.application.dtos.AuthResponseDto;
+import com.msa_delivery.auth.application.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<ApiResponseDto<?>> signIn(@RequestBody AuthRequestDto authRequestDto
+    ) {
+        return ResponseEntity.status(HttpStatus.OK.value())
+                .body(ApiResponseDto.response(200,
+                        "로그인에 성공하였습니다.",
+                        ""));
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<ApiResponseDto<? extends AuthResponseDto>> signUp(@RequestBody AuthRequestDto authRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponseDto.response(HttpStatus.CREATED.value(),
+                        "회원가입에 성공하였습니다.",
+                        authService.signUp(authRequestDto)));
+    }
+}
