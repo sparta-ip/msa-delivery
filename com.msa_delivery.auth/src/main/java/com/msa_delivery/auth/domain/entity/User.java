ea76224333b6845ecf47543115e01a2a1cd0bf4e
HongChan Yoon <hongchani0324@gmail.com>
2024-12-10 13:08:08 +0900

    [feature] #8: User entity에 생성, 수정, 삭제에 관한 필드 추가 - #8 - User entity에 생성, 수정, 삭제에 관한 필드 추가

--- com.msa_delivery.auth/src/main/java/com/msa_delivery/auth/domain/entity/User.java
+++ com.msa_delivery.auth/src/main/java/com/msa_delivery/auth/domain/entity/User.java
@@ -3,10 +3,16 @@ package com.msa_delivery.auth.domain.entity;
 import com.msa_delivery.auth.application.dtos.AuthRequestDto;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Table(name = "p_users", schema = "MEMBER")
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
@@ -16,17 +22,45 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(unique = true)
+    @Column(length = 50, nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
     private UserRoleEnum role;
 
-    @Column(name = "slack_id")
+    @Column(length = 50, nullable = false, name = "slack_id")
     private String slackId;
 
-    public static User DtoAndPasswordOf(AuthRequestDto authRequestDto, String password) {
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(length = 50, name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    @Column(length = 100, name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "deleted_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime deletedAt;
+
+    @Column(length = 100, name = "deleted_by")
+    private String deletedBy;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    public static User dtoAndPasswordOf(final AuthRequestDto authRequestDto, final String password) {
         return User.builder()
                 .username(authRequestDto.getUsername())
                 .password(password)
