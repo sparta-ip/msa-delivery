a72dda256a2202693a8ad62fa85b1bb2b4210a1b
HongChan Yoon <hongchani0324@gmail.com>
2024-12-11 00:09:37 +0900

    [feature] #8: jwt토큰의 payload의 정보를 auth 서버로 보내 실제 db의 유저와 일치하는지 확인 로직 구현. - #8 - jwt토큰의 payload의 정보를 auth 서버로 보내 실제 db의 유저와 일치하는지 확인 로직 구현.

--- com.msa_delivery.gateway/src/main/java/com/msa_delivery/gateway/infrastructure/client/AuthClient.java
+++ com.msa_delivery.gateway/src/main/java/com/msa_delivery/gateway/infrastructure/client/AuthClient.java
@@ -3,12 +3,11 @@ package com.msa_delivery.gateway.infrastructure.client;
 import com.msa_delivery.gateway.application.service.AuthService;
 import com.msa_delivery.gateway.infrastructure.dtos.VerifyUserDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "auth-service")
 public interface AuthClient extends AuthService {
-    @GetMapping("/api/auth/verify")
-        // 유저 검증 API
+    @PostMapping("/api/auth/verify")
     Boolean verifyUser(@RequestBody VerifyUserDto verifyUserDto);
 }
