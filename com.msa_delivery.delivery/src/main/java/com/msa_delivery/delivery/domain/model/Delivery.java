c6714b8c0056ac4c6d503bf5bfce82652cacded9
jelliclesu <jelliclesu@naver.com>
2024-12-11 21:07:38 +0900

    feat #41: delivery, deliveryRoute, deliveryManager 엔티티 생성

---
+++ com.msa_delivery.delivery/src/main/java/com/msa_delivery/delivery/domain/model/Delivery.java
@@ -0,0 +1,39 @@
+package com.msa_delivery.delivery.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_deliveries" , schema = "delivery")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "delivery_id")
+    private UUID id;
+
+    private UUID orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_manager_id")
+    private DeliveryManager deliveryManager;    // 업체 배송 담당자 (COMPANY_DELIVERY_MANAGER)
+
+    private Long receiverId;    // 수령인 (수령 업체의 업체 담당자)
+    private String receiverSlackId; // 수령인 슬랙 ID (수령 업체의 업체 담당자 슬랙 ID)
+
+    private UUID departureId;
+    private UUID arrivalId;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+
+    private String address;
+
+    @OneToOne(mappedBy = "delivery")
+    private DeliveryRoute deliveryRoute;
+}
---
+++ com.msa_delivery.delivery/src/main/java/com/msa_delivery/delivery/domain/model/DeliveryRoute.java
@@ -0,0 +1,40 @@
+package com.msa_delivery.delivery.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_delivery_routes" , schema = "delivery")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryRoute extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "delivery_route_id")
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_manager_id")
+    private DeliveryManager deliveryManager;    // 허브 간 배송 담당자(HUB_DELIVERY_MANAGER)
+
+    private Integer sequence;
+
+    private UUID departureId;
+    private UUID arrivalId;
+
+    private Integer expectDistance;
+    private Integer expectDuration;
+    private Integer distance;
+    private Integer duration;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+}
