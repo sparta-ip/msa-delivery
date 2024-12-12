7a0a664cd6ed6d12c6671f1d68be782bf50cb106
seven2762 <seven2762@gmail.com>
2024-12-12 22:01:48 +0900

    [feature][#86]: 기존 코드 리팩토링 및 rud 구현

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainServiceImpl.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainServiceImpl.java
@@ -1,15 +1,35 @@
 package com.msa_delivery.hub.domain.service;
 
 
+import com.msa_delivery.hub.application.dto.request.CreateHubReqDto;
+import com.msa_delivery.hub.domain.model.Hubs;
+import com.msa_delivery.hub.domain.model.Location;
+import com.msa_delivery.hub.domain.port.GeoCodingPort;
+import com.msa_delivery.hub.domain.repository.HubWriteRepository;
 import com.msa_delivery.hub.domain.repository.HubReadRepository;
+import com.msa_delivery.hub.presentation.response.HubRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class HubDomainServiceImpl implements HubDomainService {
 
     private final HubReadRepository hubReadRepository;
+    private final GeoCodingPort geoCodingPort;
+    private final HubWriteRepository hubWriteRepository;
+
+    @Transactional
+    public HubRes createHubs(CreateHubReqDto reqDto) {
+        verifyDuplicatedHub(reqDto.getHub().getName());
+        Hubs hub = reqDto.toEntity("master", geoCodingPort.getGeocode(reqDto.getHub().getAddress()));
+        Hubs savedHub = hubWriteRepository.save(hub);
+        return HubRes.from(savedHub);
+    }
 
     @Override
     public void verifyDuplicatedHub(String name) {
@@ -17,4 +37,25 @@ public class HubDomainServiceImpl implements HubDomainService {
             throw new IllegalArgumentException("이미 존재하는 허브 입니다.");
         }
     }
+    @Transactional
+    @Override
+    public HubRes updateHubs(UUID hubId, CreateHubReqDto reqDto) {
+        return hubReadRepository.findByHubId(hubId).map(hubs -> {
+            if (!hubs.getAddress().equals(reqDto.getHub().getAddress())) {
+                Location newLocation = geoCodingPort.getGeocode(reqDto.getHub().getAddress());
+                hubs.updateHubData(reqDto, reqDto.getHub().getAddress(), newLocation);
+                return HubRes.from(hubs);
+            } else {
+                throw new IllegalArgumentException("현재 주소와 동일한 주소 입니다.");
+            }
+        }).orElseThrow(() -> new IllegalArgumentException("해당 주소의 허브를 찾을 수 없습니다."));
+    }
+    @Transactional
+    @Override
+    public void deleteHubs(UUID hubId, Long userId) {
+        Hubs hubs = hubReadRepository.findByHubId(hubId).orElseThrow(() -> new IllegalArgumentException("허브를 찾을 수 없습니다."));
+        LocalDateTime localDateTime = LocalDateTime.now();
+        hubWriteRepository.updateHubToDeleted(hubId, localDateTime, userId);
+    }
+
 }
