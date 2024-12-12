a8ab18fbc676f78690a975625b1cdbb380ec18ce
seven2762 <seven2762@gmail.com>
2024-12-12 21:56:16 +0900

    [feature][#86]: 허브 생성 requestDto 수정

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/dto/request/CreateHubReqDto.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/dto/request/CreateHubReqDto.java
@@ -1,7 +1,7 @@
 package com.msa_delivery.hub.application.dto.request;
 
 import com.msa_delivery.hub.domain.model.Hubs;
-import com.msa_delivery.hub.domain.model.LocationVO;
+import com.msa_delivery.hub.domain.model.Location;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,11 +20,12 @@ public class CreateHubReqDto {
     public static class Hub {
 
         private String name;
+
         private String address;
 
     }
 
-    public Hubs toEntity(String createdBy, LocationVO location) {
+    public Hubs toEntity(String createdBy, Location location) {
         return Hubs.builder()
                 .name(hub.getName())
                 .address(hub.getAddress())
