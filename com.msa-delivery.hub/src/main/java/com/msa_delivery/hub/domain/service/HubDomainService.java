7be60aa05a28924f08cd28b1d60b580bc3f42c8a
seven2762 <seven2762@gmail.com>
2024-12-12 22:01:08 +0900

    [feature][#86]: crud 추가 및 리팩토링으로 인한 메서드 추가

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainService.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainService.java
@@ -1,6 +1,15 @@
 package com.msa_delivery.hub.domain.service;
 
+import com.msa_delivery.hub.application.dto.request.CreateHubReqDto;
+import com.msa_delivery.hub.presentation.response.HubRes;
+
+import java.util.UUID;
+
 public interface HubDomainService {
 
     void verifyDuplicatedHub(String name);
+
+    HubRes createHubs(CreateHubReqDto reqDto);
+    HubRes updateHubs(UUID hubId, CreateHubReqDto reqDto);
+    void deleteHubs(UUID hubId, Long userId);
 }
