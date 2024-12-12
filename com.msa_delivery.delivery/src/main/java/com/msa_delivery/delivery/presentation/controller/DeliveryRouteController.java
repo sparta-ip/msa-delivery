47832c6416ba36b98932765a2317f2fe397a522e
jelliclesu <jelliclesu@naver.com>
2024-12-12 21:05:17 +0900

    feat #80: deliveryRoute delete 임시 구현

--- com.msa_delivery.delivery/src/main/java/com/msa_delivery/delivery/presentation/controller/DeliveryRouteController.java
+++ com.msa_delivery.delivery/src/main/java/com/msa_delivery/delivery/presentation/controller/DeliveryRouteController.java
@@ -1,14 +1,9 @@
 package com.msa_delivery.delivery.presentation.controller;
 
 import com.msa_delivery.delivery.application.dto.CommonResponse;
-import com.msa_delivery.delivery.application.dto.DeliveryCreateDto;
-import com.msa_delivery.delivery.application.dto.DeliveryDto;
 import com.msa_delivery.delivery.application.dto.DeliveryRouteDto;
 import com.msa_delivery.delivery.application.service.DeliveryRouteService;
-import com.msa_delivery.delivery.presentation.request.DeliveryRequest;
 import com.msa_delivery.delivery.presentation.request.DeliveryRouteRequest;
-import com.msa_delivery.delivery.presentation.request.DeliveryUpdateRequest;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +27,7 @@ public class DeliveryRouteController {
         try {
             DeliveryRouteDto route = deliveryRouteService.updateRoute(deliveryRouteId, request, userId, username, role);
             return ResponseEntity.status(HttpStatus.CREATED).body(
-                    CommonResponse.success(HttpStatus.CREATED, "배송 정보 수정이 완료되었습니다.", route)
+                    CommonResponse.success(HttpStatus.CREATED, "배송 경로 정보 수정이 완료되었습니다.", route)
             );
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
@@ -40,7 +35,28 @@ public class DeliveryRouteController {
             );
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-                    CommonResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "배송 정보 수정 중 오류가 발생했습니다.")
+                    CommonResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "배송 경로 정보 수정 중 오류가 발생했습니다.")
+            );
+        }
+    }
+
+    @DeleteMapping("/{deliveryRouteId}")
+    public ResponseEntity<?> deleteRoute(@PathVariable UUID deliveryRouteId,
+                                            @RequestHeader("X-User_Id") String userId,
+                                            @RequestHeader("X-Username") String username,
+                                            @RequestHeader("X-Role") String role) {
+        try {
+            deliveryRouteService.deleteRoute(deliveryRouteId, userId, username, role);
+            return ResponseEntity.status(HttpStatus.CREATED).body(
+                    CommonResponse.success(HttpStatus.CREATED, "배송 경로 정보 삭제가 완료되었습니다.")
+            );
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                    CommonResponse.error(HttpStatus.FORBIDDEN, e.getMessage())
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    CommonResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "배송 경로 삭제 중 오류가 발생했습니다.")
             );
         }
     }
