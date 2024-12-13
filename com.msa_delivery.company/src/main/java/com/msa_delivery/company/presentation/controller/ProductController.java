6aad8eb45d06010a9bcbb8c1de97d9e93171abe4
jelliclesu <jelliclesu@naver.com>
2024-12-13 20:32:45 +0900

    fix #111: API 수정

--- com.msa_delivery.company/src/main/java/com/msa_delivery/company/presentation/controller/ProductController.java
+++ com.msa_delivery.company/src/main/java/com/msa_delivery/company/presentation/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/companies/products")
 @RequiredArgsConstructor
 public class ProductController {
 
