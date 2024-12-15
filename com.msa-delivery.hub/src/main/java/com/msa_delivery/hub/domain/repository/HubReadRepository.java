6dece22828a1046e0acd91c2de4231ca087ffbb7
seven2762 <seven2762@gmail.com>
2024-12-16 03:25:42 +0900

    [feature][#129]: 경로 생성시 이미 생성된 경로는 제외하기 위한 JPA

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/repository/HubReadRepository.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/repository/HubReadRepository.java
@@ -3,8 +3,11 @@ package com.msa_delivery.hub.domain.repository;
 
 import com.msa_delivery.hub.domain.model.Hubs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,6 +16,7 @@ public interface HubReadRepository extends JpaRepository<Hubs , UUID> {
 
     boolean existsByNameAndIsDeletedFalse(String name);
     Optional<Hubs> findByHubId(UUID hubId);
-
+    @Query("SELECT h FROM Hubs h WHERE h.isDeleted = false AND h.hubId != :hubId")
+    List<Hubs> findAllByIsDeletedFalseAndHubIdNot(@Param("hubId") UUID hubId);
 
 }
