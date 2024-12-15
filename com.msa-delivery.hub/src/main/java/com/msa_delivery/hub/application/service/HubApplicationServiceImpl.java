460a64f41166b98f7b3f3dd2c5a430003954a16d
seven2762 <seven2762@gmail.com>
2024-12-16 03:15:52 +0900

    [feature][#129]: 허브 경로 탐색 구현으로 인한 기존 허브 생성 방식 변경 및 구현

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/service/HubApplicationServiceImpl.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/service/HubApplicationServiceImpl.java
@@ -1,11 +1,21 @@
 package com.msa_delivery.hub.application.service;
 
 import com.msa_delivery.hub.application.dto.request.CreateHubReqDto;
+import com.msa_delivery.hub.application.dto.request.HubSearch;
+import com.msa_delivery.hub.domain.model.HubRoute;
+import com.msa_delivery.hub.domain.model.Hubs;
+import com.msa_delivery.hub.domain.service.HubRouteDomainService;
 import com.msa_delivery.hub.presentation.response.HubRes;
 import com.msa_delivery.hub.domain.service.HubDomainService;
+import com.msa_delivery.hub.presentation.response.HubRouteResponse;
+import com.msa_delivery.hub.presentation.response.HubWithRoutesResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -13,20 +23,61 @@ import java.util.UUID;
 public class HubApplicationServiceImpl implements HubApplicationService {
 
 
-    private final HubDomainService domainService;
+    private final HubDomainService hubDomainService;
+    private final HubRouteDomainService hubRouteDomainService;
 
+
+    @Transactional
     @Override
-    public HubRes createHub(CreateHubReqDto createHubReqDto) {
-        return domainService.createHubs(createHubReqDto);
+    public HubWithRoutesResponse createHubWithRoutes(CreateHubReqDto createHubReqDto, Long userId) {
+        Hubs createdHub = hubDomainService.createHubs(
+                createHubReqDto.getHub().getName(),
+                createHubReqDto.getHub().getAddress(),
+                userId
+        );
+        List<HubRoute> newRoutes = hubRouteDomainService.generateRoutes(createdHub, createdHub.getHubId(), userId);
+
+        return new HubWithRoutesResponse(
+                HubRes.from(createdHub),
+                newRoutes.stream()
+                        .map(HubRouteResponse::from)
+                        .toList()
+        );
     }
 
+    @Transactional
     @Override
-    public HubRes updateHub(UUID hubId , CreateHubReqDto createHubReqDto) {
-        return domainService.updateHubs(hubId,createHubReqDto);
+    public HubWithRoutesResponse updateHub(UUID hubId , CreateHubReqDto createHubReqDto, Long userId) {
+
+        Hubs updatedHub = hubDomainService.updateHub(
+                hubId,
+                createHubReqDto.getHub().getName(),
+                createHubReqDto.getHub().getAddress(),
+                userId);
+        List<HubRoute> updatedRoutes = hubRouteDomainService.updateRelatedRoutes(hubId, userId);
+
+        return new HubWithRoutesResponse(HubRes.from(updatedHub), updatedRoutes.stream().map(HubRouteResponse::from).toList());
     }
 
+    @Transactional
     @Override
     public void deleteHub(UUID hubId, Long userId) {
-        domainService.deleteHubs(hubId, userId);
+        hubDomainService.deleteHubs(hubId, userId);
+        hubRouteDomainService.deleteRelatedRoutes(hubId, userId);
     }
+
+    public Page<HubRes> searchHubs(HubSearch hubSearch, Pageable pageable) {
+        Page<Hubs> hubsPage = hubDomainService.searchHubs(
+                hubSearch.hubId(),
+                hubSearch.name(),
+                hubSearch.address(),
+                hubSearch.hubManagerId(),
+                hubSearch.isDeleted(),
+                pageable
+        );
+
+        return hubsPage.map(HubRes::from);
+    }
+
+
 }
