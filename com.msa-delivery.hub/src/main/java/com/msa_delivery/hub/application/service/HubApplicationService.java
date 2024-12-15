5729b087418298db9a337c613d24819ea53c4f03
seven2762 <seven2762@gmail.com>
2024-12-16 03:15:36 +0900

    [feature][#129]: 허브 경로 탐색 구현으로 인한 기존 허브 생성 방식 변경 및 인터페이스 메서드 추가

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/service/HubApplicationService.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/application/service/HubApplicationService.java
@@ -1,14 +1,20 @@
 package com.msa_delivery.hub.application.service;
 
 import com.msa_delivery.hub.application.dto.request.CreateHubReqDto;
+import com.msa_delivery.hub.application.dto.request.HubSearch;
 import com.msa_delivery.hub.presentation.response.HubRes;
+import com.msa_delivery.hub.presentation.response.HubWithRoutesResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface HubApplicationService {
 
-    HubRes createHub(CreateHubReqDto createHubReqDto);
-
-    HubRes updateHub(UUID hubId, CreateHubReqDto createHubReqDto);
+    Page<HubRes> searchHubs(HubSearch hubSearch, Pageable pageable);
+    HubWithRoutesResponse createHubWithRoutes(CreateHubReqDto createHubReqDto, Long userId);
+    HubWithRoutesResponse updateHub(UUID hubId, CreateHubReqDto createHubReqDto, Long userId);
     void deleteHub(UUID id, Long UserId);
+
+
 }
