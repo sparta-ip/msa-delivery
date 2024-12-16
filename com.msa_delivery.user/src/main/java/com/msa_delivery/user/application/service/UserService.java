82b8a6fbc8981aac1aabb3263c046e6db0e98163
HongChan Yoon <hongchani0324@gmail.com>
2024-12-16 18:21:44 +0900

    [refactor] #128: user softDelete 다른 서버 요청 테스트 진행 중. - #128 - user softDelete 다른 서버 요청 테스트 진행 중.

--- com.msa_delivery.user/src/main/java/com/msa_delivery/user/application/service/UserService.java
+++ com.msa_delivery.user/src/main/java/com/msa_delivery/user/application/service/UserService.java
@@ -128,16 +128,16 @@ public class UserService {
 //            }
 //        }
         // delivery manager 삭제 요청
-//        ResponseEntity<ApiResponseDto<GetUUIDDto>> deliveryManagerByUserId = deliveryService.getDeliveryManagerByUserId(user.getUserId(), headerUserId, headerUsername, role);
-//        ApiResponseDto<GetUUIDDto> deliveryManagerBody = deliveryManagerByUserId.getBody();
-//
-//        log.info("$$$$$$$$deliveryByUserId.getBody() : {}", deliveryManagerBody);
-//
-//        if (Objects.requireNonNull(deliveryManagerBody).getStatus() == HttpStatus.OK.value() || !Objects.requireNonNull(deliveryManagerBody).getData().getDeliveryManagerId().isEmpty()) {
-//            for (Long deliveryManager : deliveryManagerBody.getData().getDeliveryManagerId()) {
-//                deliveryService.softDeleteDeliveryManager(deliveryManager);
-//            }
-//        }
+        ResponseEntity<ApiResponseDto<GetUUIDDto>> deliveryManagerByUserId = deliveryService.getDeliveryManagerByUserId(user.getUserId(), headerUserId, headerUsername, role);
+        ApiResponseDto<GetUUIDDto> deliveryManagerBody = deliveryManagerByUserId.getBody();
+
+        log.info("$$$$$$$$deliveryByUserId.getBody() : {}", deliveryManagerBody);
+
+        if (Objects.requireNonNull(deliveryManagerBody).getStatus() == HttpStatus.OK.value() || !Objects.requireNonNull(deliveryManagerBody).getData().getDeliveryManagerId().isEmpty()) {
+            for (Long deliveryManager : deliveryManagerBody.getData().getDeliveryManagerId()) {
+                deliveryService.softDeleteDeliveryManager(deliveryManager);
+            }
+        }
         // company 삭제 요청
 
         // hub 삭제 요청
