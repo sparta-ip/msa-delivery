e2cd1be9618bc4cc45882cb74cb6c6c8bd3ad23e
seven2762 <seven2762@gmail.com>
2024-12-16 20:06:32 +0900

    [feature][#151] 단건 조회 로직 추가로 인한 도메인 계층 서비스 메서드 구현

--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainService.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainService.java
@@ -1,6 +1,7 @@
 package com.msa_delivery.hub.domain.service;
 
 import com.msa_delivery.hub.application.dto.request.CreateHubReqDto;
+import com.msa_delivery.hub.domain.model.HubRoute;
 import com.msa_delivery.hub.domain.model.Hubs;
 import com.msa_delivery.hub.presentation.response.HubRes;
 import org.springframework.data.domain.Page;
@@ -13,9 +14,12 @@ public interface HubDomainService {
 
     Page<Hubs> searchHubs(UUID hubId, String name, String address, Long hubManagerId, Boolean isDeleted, Pageable pageable);
     void verifyDuplicatedHub(String name);
-    Hubs createHubs(String name, String address, Long userId);
-    Hubs updateHub(UUID hubId, String name, String address, Long userId);
-    void deleteHubs(UUID hubId, Long userId);
+    Hubs createHubs(String name, String address, String username);
+    Hubs updateHub(UUID hubId, String name, String address, String userId);
+    void deleteHubs(UUID hubId, String userId);
     List<Hubs> getHubAll();
     List<Hubs> getHubByIsDeletedFalse(UUID hubId);
+    Hubs getHub(UUID hubId);
+
+
 }
--- com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainServiceImpl.java
+++ com.msa-delivery.hub/src/main/java/com/msa_delivery/hub/domain/service/HubDomainServiceImpl.java
@@ -1,12 +1,15 @@
 package com.msa_delivery.hub.domain.service;
 
 
+import com.msa_delivery.hub.domain.model.HubRoute;
 import com.msa_delivery.hub.domain.model.Hubs;
 import com.msa_delivery.hub.domain.model.Location;
 import com.msa_delivery.hub.domain.port.GeoCodingPort;
 import com.msa_delivery.hub.domain.repository.HubRepositoryCustom;
 import com.msa_delivery.hub.domain.repository.HubWriteRepository;
 import com.msa_delivery.hub.domain.repository.HubReadRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,20 +28,35 @@ public class HubDomainServiceImpl implements HubDomainService {
     private final GeoCodingPort geoCodingPort;
     private final HubWriteRepository hubWriteRepository;
     private final HubRepositoryCustom hubRepositoryCustom;
+    private static Long hubMangerId = 17L;
+
+//    @PersistenceContext
+//    private EntityManager entityManager;  // EntityManager 추가
+//
+//    private Long getNextHubManagerId() {
+//        return (Long) entityManager
+//                .createNativeQuery("SELECT NEXTVAL('hub_manager_seq')")
+//                .getSingleResult();
+//    }
+
+
     @Override
-    public Hubs createHubs(String name, String address, Long userId) {
+    public Hubs createHubs(String name, String address, String username) {
+
 
         verifyDuplicatedHub(name);
 
         Location location = geoCodingPort.getGeocode(address);
+
         Hubs hub = Hubs.builder()
                 .name(name)
+                .hubManagerId(hubMangerId++)
                 .address(address)
                 .location(location)
-                .createdBy(String.valueOf(userId))
+                .createdBy(username)
                 .createdAt(LocalDateTime.now())
                 .build();
-        return  hubWriteRepository.save(hub);
+        return hubWriteRepository.save(hub);
     }
 
 
@@ -50,11 +68,11 @@ public class HubDomainServiceImpl implements HubDomainService {
     }
 
     @Override
-    public Hubs updateHub(UUID hubId, String name, String address, Long userId) {
+    public Hubs updateHub(UUID hubId, String name, String address, String username) {
         return hubReadRepository.findByHubId(hubId).map(hubs -> {
             if (!hubs.getAddress().equals(address)) {
                 Location newLocation = geoCodingPort.getGeocode(address);
-                hubs.updateHubData(name, address, newLocation);
+                hubs.updateHubData(name, address, newLocation, username);
                 return hubs;
             } else {
                 throw new IllegalArgumentException("현재 주소와 동일한 주소 입니다.");
@@ -65,7 +83,7 @@ public class HubDomainServiceImpl implements HubDomainService {
 
 
     @Override
-    public void deleteHubs(UUID hubId, Long userId) {
+    public void deleteHubs(UUID hubId, String userId) {
         hubReadRepository.findByHubId(hubId).orElseThrow(() -> new IllegalArgumentException("허브를 찾을 수 없습니다."));
         LocalDateTime localDateTime = LocalDateTime.now();
         hubWriteRepository.updateHubToDeleted(hubId, localDateTime, userId);
@@ -80,8 +98,17 @@ public class HubDomainServiceImpl implements HubDomainService {
     public List<Hubs> getHubByIsDeletedFalse(UUID hubId) {
         return hubReadRepository.findAllByIsDeletedFalseAndHubIdNot(hubId);
     }
+
     @Override
-    public Page<Hubs> searchHubs(UUID hubId,String name, String address, Long hubManagerId, Boolean isDeleted, Pageable pageable) {
+    public Hubs getHub(UUID hubId) {
+        return hubReadRepository.findByHubId(hubId)
+                .orElseThrow(() -> new IllegalArgumentException("허브를 찾을 수 없습니다."));
+    }
+
+
+
+    @Override
+    public Page<Hubs> searchHubs(UUID hubId, String name, String address, Long hubManagerId, Boolean isDeleted, Pageable pageable) {
         return hubRepositoryCustom.searchHubs(hubId ,name, address, hubManagerId, isDeleted, pageable);
     }
 }
